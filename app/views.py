01421f42e370a3bfdc20db4f5e3c034a5f6cdf37
AnuBTS123 <anuhyareddivari@gmail.com>
2024-01-25 23:32:17 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def registration(request):
+    UFO=UserForm()
+    PFO=ProfileForm()
+    d={'UFO':UFO,'PFO':PFO}
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            send_mail('registration','Thankyou your registration is successfull',
+                      'anuhyareddivari@gmail.com',[MUFDO.email],fail_silently=False)
+            return HttpResponse('Registration Successfull')
+        else:
+            return HttpResponse('invalid data')
+    return render(request,'registration.html',d)
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))       
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile_display(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'profile_display.html',d)
